e53a4b2113ef51f92f7c3263b047dd8a655a93f9
caragiov <lucia.caragiov@gmail.com>
2024-09-20 14:39:11 +0000

    a

--- test_packages.R
+++ test_packages.R
@@ -1,6 +1,2 @@
 library(tidyverse)
-<<<<<<< HEAD
-library(RPostgres) # often used in data science
-=======
-library(RPostgres) #load many packages
->>>>>>> 285b5bef893dea7de86fe3a15020cbfa3e61c508
+library(RPostgres) # often used in data
